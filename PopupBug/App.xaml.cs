1f0979f85c0997206bf9ffea5e2bcaf4e9c78cc7
babenkovitaliy <69189298+babenkovitaliy@users.noreply.github.com>
2023-06-29 14:58:50 -0700

    More files

---
+++ PopupBug/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace PopupBug;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		//MainPage = new AppShell();
+		MainPage = new MainPage();
+	}
+}
+
---
+++ PopupBug/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace PopupBug;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		Window window = new(new SecondaryWindow());
+
+		Application.Current.OpenWindow(window);
+	}
+}
+
+
